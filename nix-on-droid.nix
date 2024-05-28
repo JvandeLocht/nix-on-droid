a56c23c616fb0149151ebdff7f6cfbee632ee322
Jan van de Locht <jan@vandelocht.uk>
2024-05-28 10:12:15 +0200

    Initial commit.

---
+++ nix-on-droid.nix
@@ -0,0 +1,71 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  # Simply install just the packages
+  environment.packages = with pkgs; [
+    zsh
+    git
+    vim # or some other editor, e.g. nano or neovim
+
+    kubectl
+    talosctl
+    kubernetes-helm
+    k9s
+    fluxcd
+    yq-go
+    kustomize
+    kubeconform
+    wget
+    curl
+    zellij
+    procps
+    killall
+    diffutils
+    findutils
+    utillinux
+    tzdata
+    tree
+    hostname
+    man
+    gnugrep
+    gnupg
+    gnused
+    gnutar
+    bzip2
+    gzip
+    xz
+    zip
+    unzip
+  ];
+  user.shell = "${pkgs.zsh}/bin/zsh";
+  terminal.font = let
+    fontPackage = pkgs.nerdfonts.override {
+      fonts = ["UbuntuMono"];
+    };
+    fontPath = "/share/fonts/truetype/NerdFonts/UbuntuMonoNerdFont-Regular.ttf";
+  in
+    fontPackage + fontPath;
+
+  # Backup etc files instead of failing to activate generation if a file already exists in /etc
+  environment.etcBackupExtension = ".bak";
+
+  # Read the changelog before changing this value
+  system.stateVersion = "23.11";
+
+  # Set up nix for flakes
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+  # Set your time zone
+  time.timeZone = "Europe/Berlin";
+
+  # Configure home-manager
+  home-manager = {
+    config = ./home.nix;
+    backupFileExtension = "hm-bak";
+    useGlobalPkgs = true;
+  };
+}
